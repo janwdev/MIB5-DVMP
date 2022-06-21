493199329b93d54a488f68bc952cd0c392fbbbea
larafra99 <lara.sofie.franke@hs-furtwangen.de>
2022-06-21 21:57:36 +0200

    Basis materials

--- Scripts/basis.py
+++ Scripts/basis.py
@@ -1,5 +1,8 @@
 import bpy
 import bmesh
+import random
+
+from Scripts.materials import Materials
 
 class Basis:
     
@@ -22,16 +25,16 @@ class Basis:
         bm.faces.new((vert2, vert2_2, vert4_2, vert4))
         bm.faces.new((vert4_2, vert4, vert3, vert3_2))
         bm.faces.new((vert1_2, vert1, vert2, vert2_2))
-    
-    @staticmethod
-    def create_basis(width,height,length,wallthickness):
-        height = height*2.2
 
+    @staticmethod
+    def create_square(width, height,length,wallthickness):
         basismesh = bpy.data.meshes.new("BasisMesh")
         basisobject = bpy.data.objects.new("Basis", basismesh)
         bpy.context.collection.objects.link(basisobject)  # put object in collection
+
         bm = bmesh.new()
         bm.from_mesh(basismesh)
+
         # left side
         Basis.__create_vert(bm,(0-wallthickness),0,0,length,0,height)
         #right side
@@ -43,3 +46,15 @@ class Basis:
 
         bm.to_mesh(basismesh)
         bm.free()
+
+        return basisobject
+
+    @staticmethod
+    def create_basis(width, height,lenght, wallthickness):
+        height = height*2.2
+
+        basis: bpy.types.object = Basis.create_square(width, height,length,wallthickness)
+        plaster: bpy.types.Material = Materials.plaster()
+        basis.data.materials.append(plaster)
+               
+Basis.create_basis()
