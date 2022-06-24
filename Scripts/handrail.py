cbce01485cac53247167e8c0ac6a7161ca412213
Yannick4815 <yannick.reiche@gmail.com>
2022-06-24 17:00:15 +0200

    Handrail mehrere und Window-Method

--- Scripts/handrail.py
+++ Scripts/handrail.py
@@ -4,6 +4,14 @@ from . materials import Materials
 
 class Handrail:
 
+    #for development on mac
+    # def create_metal_material():
+    #     metal_material = bpy.data.materials.new("Metal")
+    #     metal_material.use_nodes = True
+    #     metal_material.node_tree.nodes["Principled BSDF"].inputs[9].default_value = 0.15
+    #     metal_material.node_tree.nodes["Principled BSDF"].inputs[6].default_value = 1
+    #     return metal_material
+
     @staticmethod
     def handrail(objects, wall, scale_x, scale_y, length, height, amount):
         ##########  MESH ##########
@@ -14,32 +22,51 @@ class Handrail:
         space_length = length-mesh_length
         space_between = space_length/(amount-1)
 
+        height = height/2
         # geländer_stäbe senkrecht
-        bpy.ops.mesh.primitive_cube_add(
-            location=(scale_x, 0, height), scale=(scale_x, scale_y, height))
-        bpy.context.active_object.name = 'Geländer_Stab'
+        stab_name = "Geländer_Stab"
+        existing = bpy.context.scene.objects.get(stab_name)
+        i = 0
+        while(existing):
+            stab_name = "Geländer_Stab_" + str(i)
+            existing = bpy.context.scene.objects.get(stab_name)
+            i += 1
+
+        bpy.ops.mesh.primitive_cube_add(location=(scale_x,0,height), scale=(scale_x, scale_y, height))
+        bpy.context.active_object.name = stab_name
         bpy.ops.object.modifier_add(type='ARRAY')
-        stab = objects['Geländer_Stab']
+        stab = objects[stab_name]
         stab.modifiers['Array'].count = amount
         stab.modifiers['Array'].use_relative_offset = False
         stab.modifiers['Array'].use_constant_offset = True
         stab.modifiers['Array'].constant_offset_displace[0] = space_between
 
-        bpy.ops.object.modifier_add(type='BEVEL')
-        stab.modifiers['Bevel'].width = 0.04
-        stab.modifiers['Bevel'].segments = 12
+        bevel = False
+        if(bevel):
+            
+            bpy.ops.object.modifier_add(type='BEVEL')
+            stab_oben.modifiers['Bevel'].width = 0.02
+            stab_oben.modifiers['Bevel'].segments = 3
+
+       
         stab.data.materials.append(stab_material)
 
         # geländer_stab_oben
-
+        stab_oben_name = "Geländer_Stab_Oben"
+        existing = bpy.context.scene.objects.get(stab_oben_name)
+        i = 0
+        while(existing):
+            stab_oben_name = "Geländer_Stab_Oben_" + str(i)
+            existing = bpy.context.scene.objects.get(stab_oben_name)
+            i += 1
         bpy.ops.mesh.primitive_cube_add(location=(
             length/2, 0, (height*2) + scale_x), scale=(length/2, scale_y, scale_x))
-        bpy.context.active_object.name = 'Geländer_Stab_Oben'
-        stab_oben = objects['Geländer_Stab_Oben']
-
-        bpy.ops.object.modifier_add(type='BEVEL')
-        stab_oben.modifiers['Bevel'].width = 0.02
-        stab_oben.modifiers['Bevel'].segments = 3
+        bpy.context.active_object.name = stab_oben_name
+        stab_oben = objects[stab_oben_name]
+        if(bevel):
+            bpy.ops.object.modifier_add(type='BEVEL')
+            stab_oben.modifiers['Bevel'].width = 0.02
+            stab_oben.modifiers['Bevel'].segments = 3
 
         bpy.ops.object.select_all(action='DESELECT')  # deselect all object
 
@@ -71,3 +98,9 @@ class Handrail:
                 wallMesh.data.materials.append(glass_material)
                 wallMesh.active_material = glass_material
                 wallMesh.active_material.use_screen_refraction = True
+    @staticmethod
+    def handrail_for_window(objects, window_width, window_height):
+        Handrail.handrail(objects, False, 0.01, 0.07, window_width, window_height*0.4, round(window_width*100/8))
+
+# needed for development
+# Handrail.handrail_for_window(bpy.data.objects, 1, 2)
